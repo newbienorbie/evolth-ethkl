5469384105da50d05e7bfc9e491a786b0abfe6e5
Jaxz T <chengsoo99@gamil.com>
2024-10-05 21:53:39 +0800

    first add our own deploy in ts

---
+++ packages/hardhat/deploy/deploy.ts
@@ -0,0 +1,50 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
+
+/**
+ * Deploys a contract named "YourContract" using the deployer account and
+ * constructor arguments set to the deployer address
+ *
+ * @param hre HardhatRuntimeEnvironment object.
+ */
+
+const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
+
+    When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
+    should have sufficient balance to pay for the gas fees for contract creation.
+
+    You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
+    with a random private key in the .env file (then used on hardhat.config.ts)
+    You can run the `yarn account` command to check your balance in every network.
+  */
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  await deploy("EasyContract", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [deployer],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract to interact with it after deploying.
+  const EasyContract = await hre.ethers.getContract<Contract>("EasyContract", deployer);
+  console.log("👋 Initial greeting:", await EasyContract.greeting());
+
+    // Deploy the contract
+    // const contract = await deploy("EasyContract", {
+    //   from: deployer,
+    //   log: true,
+    // });
+    console.log("Contract deployed to:", contractDeployment.address);
+}
+
+export default deployYourContract;
+
+deployYourContract.tags = ["EasyContract"];
